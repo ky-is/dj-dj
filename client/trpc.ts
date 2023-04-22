a12e328a2d63d1f0efe5cfd07e8761a61af66e11
Kyle <kylecoburn@gmail.com>
2023-04-22 09:12:24 -0400

    🔧🏷️ allow client to see server types

--- client/trpc.ts
+++ client/trpc.ts
@@ -1,7 +1,7 @@
 import { createTRPCProxyClient, httpLink } from '@trpc/client'
 import superjson from 'superjson'
 
-import type { AppRouter } from '../server'
+import type { AppRouter } from '#s/index'
 import { TESTING } from '#c/utils'
 
 const endpointURL = TESTING ? 'http://localhost:3201' : 'https://dj-dj.netlify.app'
